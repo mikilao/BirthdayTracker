98479cff2b4b51df5f2ad5b5ded18f622effd136
Rachel <thelivedish@gmail.com>
2021-05-05 14:36:54 -0700

    finish button and css

--- birthday-list/src/App.js
+++ birthday-list/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import data from "./data";
 import List from './List';
+import MockDate from 'mockdate';
 
 
 function App() {
   const[person, setPerson] = useState(data) //set data as the state to manage the data
-
+  const date = MockDate.set("1993-03-31")
   return (
     <div className="App">
       <main>
         <section className="container">
-          <p> `{person.length}` birthdays today</p>
+          <div className="header"><h3> {person.length} birthdays today</h3>
+          <h4>{Date} </h4>
+          </div>
           <List person = {person}/>
-          <button onCLick={() => console.log('clicked')}>Clear all</button>
+          <button onClick={() => setPerson([])}>Clear all</button>
           </section>
     
       </main>
--- birthday-list/src/List.js
+++ birthday-list/src/List.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const List = () => {
+const List = ({person}) => {
   return (
     <>
       {person.map((friend) => {
           const{age, id, name, image} = friend;
           return <article key={id} className="friend">
-              <image src={image} alt={name}/>
-              <div>
+              <img src={image} alt={name}/>
+              <div className="info">
                   <h4> {name}</h4>
                   <p>{age}</p>
-
+                  <a href="https://www.amazon.com">Find a gift</a>
+                    <span />
               </div>
           </article>
       })}
